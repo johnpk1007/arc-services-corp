4a5671dba7efb88c467b0bdda36aaeb50da0714c
John <johnpk1007@gmail.com>
2024-04-16 17:05:32 +0900

    add:images

---
+++ src/images/realImage/index.js
@@ -0,0 +1,74 @@
+import drywall1 from "./drywall/drywall1.jpeg";
+import drywall2 from "./drywall/drywall2.jpeg";
+import drywall3 from "./drywall/drywall3.jpeg";
+import drywall4 from "./drywall/drywall4.jpeg";
+
+import floor1 from "./floor/floor1.jpeg";
+import floor2 from "./floor/floor2.jpeg";
+import floor3 from "./floor/floor3.jpeg";
+import floor4 from "./floor/floor4.jpeg";
+
+import wallpaper1 from "./wallpaper/wallpaper1.jpeg";
+import wallpaper2 from "./wallpaper/wallpaper2.jpeg";
+import wallpaper3 from "./wallpaper/wallpaper3.jpeg";
+import wallpaper4 from "./wallpaper/wallpaper4.jpeg";
+
+import painting1 from "./painting/painting1.jpeg";
+import painting2 from "./painting/painting2.jpeg";
+import painting3 from "./painting/painting3.jpeg";
+import painting4 from "./painting/painting4.jpeg";
+
+import repair1 from "./repair/repair1.jpeg";
+import repair2 from "./repair/repair2.jpeg";
+import repair3 from "./repair/repair3.jpeg";
+import repair4 from "./repair/repair4.jpeg";
+
+import pressure1 from "./pressure/pressure1.jpeg";
+import pressure2 from "./pressure/pressure2.jpeg";
+import pressure3 from "./pressure/pressure3.jpeg";
+import pressure4 from "./pressure/pressure4.jpeg";
+
+import janitorial1 from "./janitorial/janitorial1.jpeg";
+import janitorial2 from "./janitorial/janitorial2.jpeg";
+import janitorial3 from "./janitorial/janitorial3.jpeg";
+import janitorial4 from "./janitorial/janitorial4.jpeg";
+
+import plumbing1 from "./plumbing/plumbing1.jpeg";
+import plumbing2 from "./plumbing/plumbing2.jpeg";
+import plumbing3 from "./plumbing/plumbing3.jpeg";
+import plumbing4 from "./plumbing/plumbing4.jpeg";
+
+export {
+  drywall1,
+  drywall2,
+  drywall3,
+  drywall4,
+  floor1,
+  floor2,
+  floor3,
+  floor4,
+  wallpaper1,
+  wallpaper2,
+  wallpaper3,
+  wallpaper4,
+  painting1,
+  painting2,
+  painting3,
+  painting4,
+  repair1,
+  repair2,
+  repair3,
+  repair4,
+  pressure1,
+  pressure2,
+  pressure3,
+  pressure4,
+  janitorial1,
+  janitorial2,
+  janitorial3,
+  janitorial4,
+  plumbing1,
+  plumbing2,
+  plumbing3,
+  plumbing4,
+};
